2727a52e820e2703f769c35edc03add42623f431
Maxessien <essienmax484@gmail.com>
2025-06-22 02:21:36 +0100

    Color contrast and format updated

--- fetch.js
+++ fetch.js
@@ -1,21 +1,25 @@
-let quizDatas
-const startBtn = document.querySelector('.start')
-if (startBtn){startBtn.addEventListener('click', ()=>{
-    console.log('fetching')
-    startBtn.textContent = 'Loading ...'
-    fetch('https://raw.githubusercontent.com/Maxessien/Test-API-Fetch-/main/test.json')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            quizDatas = data;
-            sessionStorage.setItem('quizData', JSON.stringify(quizDatas))
-            
-            window.location.href = 'quiz.html'
+let quizDatas;
+const startBtn = document.querySelector(".start");
+if (startBtn) {
+  startBtn.addEventListener("click", () => {
+    console.log("fetching");
+    startBtn.textContent = "Loading ...";
+    fetch(
+      "https://raw.githubusercontent.com/Maxessien/Test-API-Fetch-/main/test.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        quizDatas = data;
+        sessionStorage.setItem("quizData", JSON.stringify(quizDatas));
 
-        }).catch(err =>{
-            if (err.message==='Failed to fetch') {
-                alert('Network Error. Please check your internet connection')
-                startBtn.textContent = 'Start Quiz'
-            }
-        });
-})}
+        window.location.href = "quiz.html";
+      })
+      .catch((err) => {
+        if (err.message === "Failed to fetch") {
+          alert("Network Error. Please check your internet connection");
+          startBtn.textContent = "Start Quiz";
+        }
+      });
+  });
+}
--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const quizQuestions = document.querySelector('.quiz-questions')
-const quizNavigation = document.querySelector('.quiz-navigation')
-const prevBtn = document.querySelector('.prev-btn')
-const nextBtn = document.querySelector('.next-btn')
-const submitBtn = document.querySelector('.submit-btn')
-const timer = document.querySelector('.timer')
-let questionsHtml = []
-let selectedAnswers = []
-let allInputs
-let currentQuestion = 0
-let total = 0
-let time = 1800
-let answers
+const quizQuestions = document.querySelector(".quiz-questions");
+const quizNavigation = document.querySelector(".quiz-navigation");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const submitBtn = document.querySelector(".submit-btn");
+const timer = document.querySelector(".timer");
+let questionsHtml = [];
+let selectedAnswers = [];
+let allInputs;
+let currentQuestion = 0;
+let total = 0;
+let time = 1800;
+let answers;
 
-const data = sessionStorage.getItem('quizData')
+const data = sessionStorage.getItem("quizData");
 
-const quizDatas = JSON.parse(data)
+const quizDatas = JSON.parse(data);
 
 // const quizDatas = [
 //     {
@@ -60,188 +60,210 @@ const quizDatas = JSON.parse(data)
 //              'jjjwsiddjdifi0eoeldodldldldododd neque non atque.']
 //     }
 // ]
-// let answers = ['C. 1960', 'A. Aang', 'A. Max Essien', 'A. Aang', 
-//                 'A. Max Essien', 'A. Aang', 'A. Max Essien', 'A. Aang', 
+// let answers = ['C. 1960', 'A. Aang', 'A. Max Essien', 'A. Aang',
+//                 'A. Max Essien', 'A. Aang', 'A. Max Essien', 'A. Aang',
 //                 'A. Max Essien']
 
+loadNavigation();
+const navBoxes = document.querySelectorAll(".boxes");
 
-loadNavigation()
-const navBoxes = document.querySelectorAll('.boxes')
-
-function runTimer(){
-    const intervalId = setInterval(()=>{
-                            time--
-                            let seconds = time % 60
-                            let minute = Math.floor(time / 60)
-                            timer.textContent = 
-                            `${minute.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
-                            if (time===0) {
-                                clearInterval(intervalId)
-                                fetch('https://raw.githubusercontent.com/Maxessien/Test-API-Fetch-/main/answer.json').then(res=>{
-                                    return res.json()
-                                }).then(data=>{
-                                    answers = data
-                                    displayResults()
-                                })
-                            }
-                        }, 1000)
+function runTimer() {
+  const intervalId = setInterval(() => {
+    time--;
+    let seconds = time % 60;
+    let minute = Math.floor(time / 60);
+    timer.textContent = `${minute.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+    if (time === 0) {
+      clearInterval(intervalId);
+      fetch(
+        "https://raw.githubusercontent.com/Maxessien/Test-API-Fetch-/main/answer.json"
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          answers = data;
+          displayResults();
+        });
+    }
+  }, 1000);
 }
 
-function loadQuestions(){
-    if (currentQuestion === quizDatas.length - 1) {
-        elementDisplay([{target: nextBtn, action: 'remove'},
-            {target: submitBtn, action: 'add'}])
-    }else if (currentQuestion < quizDatas.length - 1) {
-        elementDisplay([{target: nextBtn, action: 'add'},
-            {target: submitBtn, action: 'remove'}])
-    }
-    if (currentQuestion === 0) {
-        elementDisplay([{target: prevBtn, action: 'remove'}])
-    }else if (currentQuestion > 0) {
-        elementDisplay([{target: prevBtn, action: 'add'}])
-    }
-    quizQuestions.innerHTML = questionsHtml[currentQuestion]
-    displayCurrentQuestion()
-    enableInputSelection()
+function loadQuestions() {
+  if (currentQuestion === quizDatas.length - 1) {
+    elementDisplay([
+      { target: nextBtn, action: "remove" },
+      { target: submitBtn, action: "add" },
+    ]);
+  } else if (currentQuestion < quizDatas.length - 1) {
+    elementDisplay([
+      { target: nextBtn, action: "add" },
+      { target: submitBtn, action: "remove" },
+    ]);
+  }
+  if (currentQuestion === 0) {
+    elementDisplay([{ target: prevBtn, action: "remove" }]);
+  } else if (currentQuestion > 0) {
+    elementDisplay([{ target: prevBtn, action: "add" }]);
+  }
+  quizQuestions.innerHTML = questionsHtml[currentQuestion];
+  displayCurrentQuestion();
+  enableInputSelection();
 }
 
-function createQuestions(){
-    quizDatas.forEach((quizData, index)=>{
-        selectedAnswers[index] = 'No answer'
-        questionsHtml.push(
-            `<div class="question">
+function createQuestions() {
+  quizDatas.forEach((quizData, index) => {
+    selectedAnswers[index] = "No answer";
+    questionsHtml.push(
+      `<div class="question">
                 ${quizData.question}
             </div>
             <ul class="options">
-                ${
-                    quizData.options.map((element, i)=>{
-                        return `<li class="options-list">
-                                    <input type="radio" class="no-display current-inputs" value="${element}" name="question${currentQuestion+1}options" id="opt${i+1}">
-                                    <label for="opt${i+1}">${element}</label>
-                                </li>`
-                    }).join('')
-                }
+                ${quizData.options
+                  .map((element, i) => {
+                    return `<li class="options-list">
+                                    <input type="radio" class="no-display current-inputs" value="${element}" name="question${
+                      currentQuestion + 1
+                    }options" id="opt${i + 1}">
+                                    <label for="opt${i + 1}">${element}</label>
+                                </li>`;
+                  })
+                  .join("")}
             </ul>`
-        )
-    })
-    console.log(selectedAnswers)
+    );
+  });
+  console.log(selectedAnswers);
 }
 
+function elementDisplay(arr) {
+  arr.forEach((element) => {
+    const target = element.target;
+    const action = element.action;
+    if (action === "add") {
+      target.classList.remove("no-display");
+    } else if (action === "remove") {
+      target.classList.add("no-display");
+    } else {
+      console.error(`${action} is not a valid action`);
+    }
+  });
+}
 
-function elementDisplay(arr){
-    arr.forEach(element => {
-        const target = element.target
-        const action = element.action
-        if (action==='add') {
-            target.classList.remove('no-display')
-        }else if (action==='remove') {
-            target.classList.add('no-display')
-        }else{
-            console.error(`${action} is not a valid action`)
-        }
+function selectedOption() {
+  let optionSelected = false;
+  const currentInputs = document.querySelectorAll(".current-inputs");
+  currentInputs.forEach((input) => {
+    if (
+      document
+        .querySelector(`label[for="${input.id}"]`)
+        .classList.contains("selected")
+    ) {
+      selectedAnswers[currentQuestion] = input.value;
+      optionSelected = true;
+    }
+  });
+  if (!optionSelected) {
+    selectedAnswers[currentQuestion] = "No answer";
+  }
+  updateQuizNavigation(optionSelected);
+  console.log(selectedAnswers);
+}
+
+function nextQuestion() {
+  questionsHtml[currentQuestion] = quizQuestions.innerHTML;
+  currentQuestion++;
+  loadQuestions();
+}
+
+function previousQuestion() {
+  questionsHtml[currentQuestion] = quizQuestions.innerHTML;
+  currentQuestion--;
+  loadQuestions();
+}
+
+function enableInputSelection() {
+  allInputs = document.querySelectorAll("input");
+  allInputs.forEach((input) => {
+    input.addEventListener("change", () => {
+      console.log("chosen");
+      allInputs.forEach((inputs) => {
+        document
+          .querySelector(`label[for="${inputs.id}"]`)
+          .classList.remove("selected");
+      });
+      if (input.checked) {
+        document
+          .querySelector(`label[for="${input.id}"]`)
+          .classList.add("selected");
+        selectedOption();
+      }
     });
+  });
 }
 
-function selectedOption(){
-    let optionSelected = false
-    const currentInputs = document.querySelectorAll('.current-inputs')
-    currentInputs.forEach(input=>{
-        if (document.querySelector(`label[for="${input.id}"]`).classList.contains('selected')) {
-            selectedAnswers[currentQuestion] = input.value
-            optionSelected = true
-        }
+function loadNavigation() {
+  quizNavigation.innerHTML = quizDatas
+    .map((quizData, index) => {
+      return `<div class="boxes">${index + 1}</div>`;
     })
-    if (!optionSelected) {
-        selectedAnswers[currentQuestion] = 'No answer'
+    .join("");
+}
+
+function updateQuizNavigation(bool) {
+  if (bool) {
+    navBoxes[currentQuestion].classList.add("answered");
+  }
+}
+
+function skipToQuestion(number) {
+  currentQuestion = number;
+  loadQuestions();
+}
+
+function displayResults() {
+  total = 0;
+  answers.forEach((ans, index) => {
+    if (ans.trim() === selectedAnswers[index].trim()) {
+      total++;
     }
-    updateQuizNavigation(optionSelected)
-    console.log(selectedAnswers)
+  });
+  sessionStorage.setItem("score", total);
+  sessionStorage.setItem("length", answers.length);
+  sessionStorage.setItem("answersArr", JSON.stringify(answers));
+  sessionStorage.setItem("selectedAnswers", JSON.stringify(selectedAnswers));
+  window.location.replace("results.html");
 }
 
-function nextQuestion(){
-    questionsHtml[currentQuestion] = quizQuestions.innerHTML
-    currentQuestion++
-    loadQuestions()
-}
-
-function previousQuestion(){
-    questionsHtml[currentQuestion] = quizQuestions.innerHTML
-    currentQuestion--
-    loadQuestions()
-}
-
-function enableInputSelection(){
-    allInputs = document.querySelectorAll('input')
-    allInputs.forEach(input=>{
-        input.addEventListener('change', ()=>{
-            console.log('chosen')
-            allInputs.forEach(inputs=>{
-                document.querySelector(`label[for="${inputs.id}"]`).classList.remove('selected')
-            })
-            if (input.checked) {
-                document.querySelector(`label[for="${input.id}"]`).classList.add('selected')
-                selectedOption()
-            }
-        })
+submitBtn.addEventListener("click", () => {
+  fetch(
+    "https://raw.githubusercontent.com/Maxessien/Test-API-Fetch-/main/answer.json"
+  )
+    .then((res) => {
+      return res.json();
     })
+    .then((ansData) => {
+      answers = ansData;
+      displayResults();
+    });
+});
+createQuestions();
+loadQuestions();
+runTimer();
+nextBtn.addEventListener("click", nextQuestion);
+prevBtn.addEventListener("click", previousQuestion);
+
+function displayCurrentQuestion() {
+  navBoxes.forEach((navBox) => {
+    navBox.classList.remove("current");
+  });
+  navBoxes[currentQuestion].classList.add("current");
+  console.log(navBoxes[currentQuestion]);
 }
 
-function loadNavigation(){
-    quizNavigation.innerHTML = quizDatas.map((quizData, index)=>{
-        return `<div class="boxes">${index+1}</div>`
-    }).join('')
-}
-
-function updateQuizNavigation(bool){
-    if (bool) {
-        navBoxes[currentQuestion].classList.add('answered')
-    }
-}
-
-function skipToQuestion(number){
-    currentQuestion=number
-    loadQuestions()
-}
-
-function displayResults(){
-    total = 0
-    answers.forEach((ans, index)=>{
-        if (ans.trim()===selectedAnswers[index].trim()) {
-            total++
-        }
-    })
-    sessionStorage.setItem('score', total)
-    sessionStorage.setItem('length', answers.length)
-    sessionStorage.setItem('answersArr', JSON.stringify(answers))
-    sessionStorage.setItem('selectedAnswers', JSON.stringify(selectedAnswers))
-    window.location.replace('results.html')
-}
-
-submitBtn.addEventListener('click', ()=>{
-    fetch('https://raw.githubusercontent.com/Maxessien/Test-API-Fetch-/main/answer.json').then(res=>{
-                return res.json()
-            }).then(ansData=>{
-                answers = ansData
-                displayResults()
-            })
-    })
-createQuestions()
-loadQuestions()
-runTimer()
-nextBtn.addEventListener('click', nextQuestion)
-prevBtn.addEventListener('click', previousQuestion)
-
-function displayCurrentQuestion(){
-    navBoxes.forEach(navBox=>{
-        navBox.classList.remove('current')
-    })
-    navBoxes[currentQuestion].classList.add('current')
-    console.log(navBoxes[currentQuestion])
-}
-
-
-navBoxes.forEach((navBox, index)=>{
-    navBox.addEventListener('click', ()=>{
-        skipToQuestion(index)
-    })
-})
+navBoxes.forEach((navBox, index) => {
+  navBox.addEventListener("click", () => {
+    skipToQuestion(index);
+  });
+});
